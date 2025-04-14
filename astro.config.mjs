231d2b31fb274a0f267364ffb012dab2ce8dfef7
Seb <bones@ucc.asn.au>
2025-04-14 13:13:55 +0800

    removed cdn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,67 +1,78 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
-import astroExpressiveCode, { ExpressiveCodeTheme } from 'astro-expressive-code'
+import astroExpressiveCode, {
+  ExpressiveCodeTheme,
+} from "astro-expressive-code";
 
-import fs from 'node:fs';
+import fs from "node:fs";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import { toString } from 'hast-util-to-string'
+import { toString } from "hast-util-to-string";
 
-import { h } from 'hastscript'
+import { h } from "hastscript";
 
-import autolinkHeadings from 'rehype-autolink-headings'
+import autolinkHeadings from "rehype-autolink-headings";
 
-import opengraphImage from 'astro-opengraph-image';
+import opengraphImage from "astro-opengraph-image";
 
 import { readFile } from "node:fs/promises";
 // const jsoncStringDark = fs.readFileSync(new URL(`./dark.jsonc`, import.meta.url), 'utf-8')
 // const dark = ExpressiveCodeTheme.fromJSONString(jsoncStringDark)
 // const jsoncStringLight = fs.readFileSync(new URL(`./light.jsonc`, import.meta.url), 'utf-8')
 // const light = ExpressiveCodeTheme.fromJSONString(jsoncStringLight)
-const jsoncStringDark = fs.readFileSync(new URL(`./tokyonightdark.jsonc`, import.meta.url), 'utf-8')
-const dark = ExpressiveCodeTheme.fromJSONString(jsoncStringDark)
-const jsoncStringLight = fs.readFileSync(new URL(`./tokyonightlight.jsonc`, import.meta.url), 'utf-8')
-const light = ExpressiveCodeTheme.fromJSONString(jsoncStringLight)
+const jsoncStringDark = fs.readFileSync(
+  new URL(`./tokyonightdark.jsonc`, import.meta.url),
+  "utf-8",
+);
+const dark = ExpressiveCodeTheme.fromJSONString(jsoncStringDark);
+const jsoncStringLight = fs.readFileSync(
+  new URL(`./tokyonightlight.jsonc`, import.meta.url),
+  "utf-8",
+);
+const light = ExpressiveCodeTheme.fromJSONString(jsoncStringLight);
 // https://astro.build/config
 const createSROnlyLabel = (text) => {
-  const node = h('span.sr-only', `Section titled ${escape(text)}`)
-  node.properties['is:raw'] = true
-  return node
-}
+  const node = h("span.sr-only", `Section titled ${escape(text)}`);
+  node.properties["is:raw"] = true;
+  return node;
+};
 
 export default defineConfig({
-  site: 'https://mahoosively.gay',
+  site: "https://mahoosively.gay",
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
 
   scopedStyleStrategy: "attribute",
 
-  integrations: [astroExpressiveCode({
-    themes: [dark, light],
-    // themes: ['catppuccin-latte', 'catppuccin-mocha', "catppuccin-macchiato", 'tokyo-night', 'material-theme-palenight'],
-    minSyntaxHighlightingColorContrast: 0,
-    useThemedSelectionColors: true,
-    
-    styleOverrides: {
-      // You can also override styles
-      borderRadius: '0.5rem',
-      borderWidth: '2px',
-      codeFontFamily: "JetBrains Mono",
-      uiFontFamily: 'Fira Code'
-    }
-  
-    
-  }), mdx(), sitemap(), react(), opengraphImage(
-  { // what size do you want your images to be?
+  integrations: [
+    astroExpressiveCode({
+      themes: [dark, light],
+      // themes: ['catppuccin-latte', 'catppuccin-mocha', "catppuccin-macchiato", 'tokyo-night', 'material-theme-palenight'],
+      minSyntaxHighlightingColorContrast: 0,
+      useThemedSelectionColors: true,
+
+      styleOverrides: {
+        // You can also override styles
+        borderRadius: "0.5rem",
+        borderWidth: "2px",
+        codeFontFamily: "JetBrains Mono",
+        uiFontFamily: "Fira Code",
+      },
+    }),
+    mdx(),
+    sitemap(),
+    react(),
+    opengraphImage({
+      // what size do you want your images to be?
       // 1200x630 is a good default across platforms,
       // and 3x scale is a convenient choice.
       width: 1200,
@@ -88,10 +99,15 @@ export default defineConfig({
           weight: 700,
         },
       ],
-
-  })],
+    }),
+  ],
 
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
+    middleware: async (request, response, next) => {
+      // Add security headers
+      response.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+      await next();
+    },
   }),
 });
