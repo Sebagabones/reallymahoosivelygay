eab4f8d6da80b9016724650c2b9277291143f6fc
Seb <bones@ucc.asn.au>
2025-05-17 23:00:39 +0800

    heres hoping fr

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
+
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
   schema: z.object({
