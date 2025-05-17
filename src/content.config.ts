06ad27d87593b57f8d9c6b79d8307fdd6c3cc8a6
Seb <bones@ucc.asn.au>
2025-05-17 23:03:14 +0800

    rolling back chat

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
   schema: z.object({
