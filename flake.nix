c5c7a471c1d79bc14712b1e3f0784225f9778ce7
Sebgabones <133339614+Sebagabones@users.noreply.github.com>
2025-11-23 16:27:25 +0800

    fix css

--- flake.nix
+++ flake.nix
@@ -1,20 +1,20 @@
 {
-  description = "Nix by Example";
+  description = "Mahoosively Gay";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     bun2nix.url = "github:baileyluTCD/bun2nix";
     bun2nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, bun2nix }:
+  outputs = { self, nixpkgs, bun2nix, flake-utils }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       nodejs = pkgs.nodejs_24;
       bun = pkgs.bun;
       mkBunDerivation = bun2nix.lib.${system}.mkBunDerivation;
-      # bun2nix = bun2nix.packages.${system}.default;
     in {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
@@ -28,9 +28,6 @@
           export PATH="$PWD/node_modules/.bin:$PATH"
         '';
       };
-      # mahoosivelyGay = pkgs.callPackage ./build.nix {
-      #   inherit (bun2nix.lib.${system}) mkBunDerivation;
-      # };
       packages.${system}.default = mkBunDerivation {
         pname = "MahoosivelyGay";
         packageJson = ./package.json;
@@ -43,31 +40,7 @@
         installPhase = ''
           cp -r dist $out
         '';
-        # pname = "Mahoosively Gay";
-        # version = "1.0.0";
-        #
-        # src = ./.;
-        #
-        # bunNix = ./bun.nix;
-        #
-        # index = "client/index.html";
       };
-      # packages.${system}.default = pkgs.stdenv.mkDerivation (finalAttrs: {
-      #   pname = "astro-site";
-      #   version = "0.0.1";
-      #   src = ./.;
-      #
-      #   nativeBuildInputs = [ nodejs bun ];
-      #
-      #   buildPhase = ''
-      #     bun run build
-      #   '';
-      #
-      #   installPhase = ''
-      #     cp -r dist $out
-      #   '';
-      # });
-
-      formatter = pkgs.nixpkgs-fmt;
+      formatter.${system} = pkgs.nixpkgs-fmt;
     };
 }
